89f979f62ed3c5c0f09e155241cab8b5e00f15d7
Shirade <pitscotti02@gmail.com>
2024-04-30 14:58:01 +0200

    risolto

--- prova/src/prova/Main.java
+++ prova/src/prova/Main.java
@@ -5,14 +5,7 @@ import java.io.*;
 public class Main {
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("h");
-=======
-
-		System.out.println("helpit");
-
-		
-
->>>>>>> cb39cad67eb37f04b4f9644409b43a39e93481f6
 	}
 }
